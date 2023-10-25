5606012004e7358731980a38dceb69cfea4bac0c
Keith Cheong <keithcheongwaikeong@gmail.com>
2023-10-26 02:37:03 +0800

    Update logic to test FL0 deployment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import { Telegraf } from "telegraf";
+import { message } from 'telegraf/filters'
 
 require('dotenv').config();
 
@@ -17,20 +18,22 @@ if (!isLocal) {
   bot.telegram.setWebhook(url);
 }
 
-console.log('Adding Telegram bot logic...');
+console.log('Implementing Telegram bot logic...');
 
 bot.start((ctx) => {
   ctx.reply(`
-  Hello ${ctx.from.first_name}!\nWelcome to Keith's pilot Tele Bot!\nYou can use /help and /hello, type something or send a photo.`);
+  Hello ${ctx.from.first_name}!\nWelcome to Keith's pilot Tele Bot!\nYou can use /help and /hello, type something or send a photo/video.`);
 })
 
-bot.help((ctx) => ctx.reply("I don't really want to..."));
+bot.help((ctx) => ctx.reply('Have you tried turning it off and on again?'));
 
 bot.command('hello', (ctx) => ctx.reply('Hello again!'));
 
-bot.on('text', (ctx) => ctx.reply(`You have sent: ${ctx.message.text}`));
+bot.on(message('text'), (ctx) => ctx.reply(`You have sent: ${ctx.message.text}... Not really helpful at this point`));
 
-bot.on('photo', (ctx) => ctx.reply('You look... okay'));
+bot.on(message('photo'), (ctx) => ctx.reply('It looks... okay'));
+
+bot.on(message('video'), (ctx) => ctx.reply('What... do I do with this?'));
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}...`);
