84d47a4d958019e0737082380a3d8064a0f47019
KeithCheongWaiKeong <keithcheongwaikeong@gmail.com>
2024-02-28 17:23:28 +0800

    Run prettier

--- src/scenes/contants.ts
+++ src/scenes/contants.ts
@@ -1,29 +1,29 @@
 const STATION_CODE_SCENE_ID = "stationCode";
 
 const TEAM_PATHS = [
-  [1,2,3,0],
-  [1,2,0,3],
-  [1,3,2,0],
-  [1,3,0,2],
-  [1,0,2,3],
-  [1,0,3,2],
-  [2,1,3,0],
-  [2,1,0,3],
-  [2,3,1,0],
-  [2,3,0,1],
-  [2,0,1,3],
-  [2,0,3,1],
-  [3,1,2,0],
-  [3,1,0,2],
-  [3,2,1,0],
-  [3,2,0,1],
+  [1, 2, 3, 0],
+  [1, 2, 0, 3],
+  [1, 3, 2, 0],
+  [1, 3, 0, 2],
+  [1, 0, 2, 3],
+  [1, 0, 3, 2],
+  [2, 1, 3, 0],
+  [2, 1, 0, 3],
+  [2, 3, 1, 0],
+  [2, 3, 0, 1],
+  [2, 0, 1, 3],
+  [2, 0, 3, 1],
+  [3, 1, 2, 0],
+  [3, 1, 0, 2],
+  [3, 2, 1, 0],
+  [3, 2, 0, 1],
 ];
 
 const STATION_IDS = {
-  "HALL": 0,
-  "CLASS": 1,
-  "AUDI": 2,
-  "LOUNGE": 3,
+  HALL: 0,
+  CLASS: 1,
+  AUDI: 2,
+  LOUNGE: 3,
 };
 
 const STATION_HINTS = [
@@ -33,9 +33,4 @@ const STATION_HINTS = [
   "It's at Lounge",
 ];
 
-export {
-  STATION_CODE_SCENE_ID,
-  TEAM_PATHS,
-  STATION_IDS,
-  STATION_HINTS
-}
+export { STATION_CODE_SCENE_ID, TEAM_PATHS, STATION_IDS, STATION_HINTS };
